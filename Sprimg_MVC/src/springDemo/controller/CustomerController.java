4e3fdfd1811581829a709cf7ddce4c7b82914332
Semih Yıldız <sdsemihyildiz@gmail.com>
2022-11-15 06:59:25 +0300

    validations

---
+++ Sprimg_MVC/src/springDemo/controller/CustomerController.java
@@ -0,0 +1,51 @@
+package springDemo.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import springDemo.Customer;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	// add an initbinder to convert trim input strings
+	// remove leading and trailing whitespace
+	// resolve issue for our validation
+
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+
+	@RequestMapping("/showForm")
+	public String showForm(Model theModel) {
+		theModel.addAttribute("customer", new Customer());
+		return "customer-form";
+	}
+
+	@RequestMapping("/processForm")
+	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+		// @Valid: Perform validation rules on Customer object
+		// Results of validation placed in the BindingResult
+		// if there is an error get back to the customer-form
+		// else go the the validation
+		System.out.println("Last Name: |" + theCustomer.getLastName() + "|");
+		if (theBindingResult.hasErrors()) {
+			return "customer-form";
+		} else {
+			return "customer-validation";
+		}
+
+	}
+
+}
